fcf7989754fbdfac5284d79e68ca86278f4e6f4d
Francisco A. Lozano <flozano@gmail.com>
2016-03-29 18:43:29 +0200

    Now use user credentials

--- createandupdate.go
+++ createandupdate.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"bytes"
 	"math/rand"
+	"time"
 )
 
 var logger *log.Logger
@@ -22,7 +23,89 @@ func randomString(strlen int) string {
 	return string(result)
 }
 
-func login(url string, appID string, appKey string, clientID string, clientSecret string) string {
+func register(url string, appID string, appKey string) (userID string, token string) {
+	type regAndAuthRequest struct {
+		UserName string `json:"loginName"`
+		Password string `json:"password"`
+	}
+
+	type regAndAuthResponse struct {
+		UserID      string `json:"userID"`
+		AccessToken string `json:"_accessToken"`
+	}
+
+	requestBody := regAndAuthRequest{UserName: randomString(10), Password: randomString(10), }
+	jsonRequest, _ := json.Marshal(requestBody)
+
+	logger.Printf("Registration request is %s", string(jsonRequest))
+
+	req, err := http.NewRequest("POST", url + "/apps/"+appID+"/users", bytes.NewReader(jsonRequest))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	req.Header.Add("x-kii-appid", appID)
+	req.Header.Add("x-kii-appkey", appKey)
+	req.Header.Add("content-type", "application/vnd.kii.RegistrationAndAuthorizationRequest+json")
+	req.Header.Add("accept", "application/vnd.kii.RegistrationAndAuthorizationResponse+json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if (res.StatusCode != 201) {
+		logger.Fatal("Registration failed ", res.Status, string(body))
+	}
+	var response regAndAuthResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	logger.Printf("Registered (response=%s)", string(body))
+	return response.UserID, response.AccessToken
+}
+
+func userLogin(url string, appID string, appKey string, userName string, password string) string {
+	type oauthRequest struct {
+		UserName string `json:"username"`
+		Password string `json:"password"`
+	}
+	type oauthResponse struct {
+		AccessToken string `json:"access_token"`
+	}
+
+	requestBody := oauthRequest{UserName: userName, Password: password, }
+	jsonRequest, _ := json.Marshal(requestBody)
+
+	logger.Printf("Log-in request is %s", string(jsonRequest))
+
+	req, err := http.NewRequest("POST", url + "/oauth2/token", bytes.NewReader(jsonRequest))
+	if err != nil {
+		panic(err.Error())
+	}
+
+	req.Header.Add("x-kii-appid", appID)
+	req.Header.Add("x-kii-appkey", appKey)
+	req.Header.Add("content-type", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if (res.StatusCode != 200) {
+		logger.Fatal("Log-in failed ", res.Status, string(body))
+	}
+	var response oauthResponse
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	logger.Printf("Logged-in as admin (token=%s)", response.AccessToken)
+	return response.AccessToken
+}
+func adminLogin(url string, appID string, appKey string, clientID string, clientSecret string) string {
 
 	type oauthRequest struct {
 		ClientID     string `json:"client_id"`
@@ -60,7 +143,7 @@ func login(url string, appID string, appKey string, clientID string, clientSecre
 	if err != nil {
 		logger.Fatal(err.Error())
 	}
-	logger.Printf("Logged-in (token=%s)", response.AccessToken)
+	logger.Printf("Logged-in as admin (token=%s)", response.AccessToken)
 	return response.AccessToken
 
 }
@@ -83,7 +166,7 @@ func updateObject(url string, token string, appID string, appKey string, objectI
 		FirstName: randomString(20),
 		SomeOtherStuff: randomString(45),
 	})
-	req, err := http.NewRequest("PATCH", url + "/apps/" + appID + "/buckets/testing_kii/objects/" + objectID, bytes.NewReader(jsonRequest))
+	req, err := http.NewRequest("PATCH", url + "/apps/" + appID + "/users/me/buckets/testing_kii/objects/" + objectID, bytes.NewReader(jsonRequest))
 	if err != nil {
 		log.Fatal(err.Error())
 	}
@@ -130,7 +213,7 @@ func createObject(url string, token string, appID string, appKey string) string 
 		EmailAddress: randomString(25),
 	})
 
-	req, err := http.NewRequest("POST", url + "/apps/" + appID + "/buckets/testing_kii/objects", bytes.NewReader(jsonRequest))
+	req, err := http.NewRequest("POST", url + "/apps/" + appID + "/users/me/buckets/testing_kii/objects", bytes.NewReader(jsonRequest))
 	if err != nil {
 		logger.Fatal(err.Error())
 	}
@@ -155,21 +238,20 @@ func createObject(url string, token string, appID string, appKey string) string 
 }
 
 func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	logger = log.New(os.Stdout, "createandupdate - ", log.LstdFlags | log.Lmicroseconds)
 	client = &http.Client{}
 }
 
 func main() {
-	if len(os.Args) != 6 {
-		log.Fatal("Incorrect usage. Usage: url appID appKey clientID clientSecret")
+	if len(os.Args) != 4 {
+		log.Fatal("Incorrect usage. Usage: url appID appKey")
 	}
 	url := os.Args[1]
 	appID := os.Args[2]
 	appKey := os.Args[3]
-	clientID := os.Args[4]
-	clientSecret := os.Args[5]
-	logger.Printf("Settings: url=%s, appID=%s, appKey=%s, clientID=%s, clientSecret=%s", url, appID, appKey, clientID, clientSecret)
-	token := login(url, appID, appKey, clientID, clientSecret)
+	logger.Printf("Settings: url=%s, appID=%s, appKey=%s", url, appID, appKey)
+	_, token := register(url, appID, appKey)
 	for {
 		updateObject(url, token, appID, appKey, createObject(url, token, appID, appKey))
 	}
